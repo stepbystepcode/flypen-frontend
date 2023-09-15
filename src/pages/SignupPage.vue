f68f4a3c94283c80344545c9ef4630bf5e40de1a
Li Jing <3202146864@qq.com>
2023-09-15 18:45:44 +0800

    update ui

--- src/pages/SignupPage.vue
+++ src/pages/SignupPage.vue
@@ -1,20 +1,47 @@
 <template>
-    <div class="column flex-center fullscreen q-gutter-md">
-    <q-input outlined v-model="username" label="username"></q-input>
-    <q-input outlined v-model="password" label="password"></q-input>
-    <q-btn @click="signup">注册</q-btn>
-</div></template>
+  <div style="width: 80vw; margin: 0 auto">
+    <div class="column flex-center q-gutter-md">
+      <div class="text-h4 q-pt-md">Flypen</div>
+      <div class="text-h5">Sign in</div>
+      <span>Use your Flypen Account</span>
+      <q-input
+        outlined
+        v-model="username"
+        style="width: 80vw"
+        label="username"
+      ></q-input>
+      <q-input
+        outlined
+        v-model="password"
+        style="width: 80vw"
+        label="password"
+      ></q-input>
+      <div class="row flex-center justify-between" style="width: 95%">
+        <div><a href="/signup" class="text-primary text-bold" style="text-decoration: none">Create account</a></div>
+        <q-btn @click="login" class="bg-primary text-white text-bold">登录</q-btn>
+      </div>
+    </div>
+  </div>
+</template>
 <script setup>
 import axios from 'axios';
-import {ref} from 'vue'
-import Swal from 'sweetalert2'
-const username=ref('');
-const password=ref('');
-const signup = () => {
-    axios.post('http://192.168.41.231:8081/api/signup',{username:username.value,password:password.value}).then((res)=>{
-        Swal.fire(res.data.msg);
+import { ref } from 'vue';
+import Swal from 'sweetalert2';
+const username = ref('');
+const password = ref('');
+const login = () => {
+  axios
+    .post('http://127.0.0.1:8081/api/signup', {
+      username: username.value,
+      password: password.value,
+    })
+    .then((res) => {
+      Swal.fire({
+        title: res.data.msg,
+        icon: res.data.msg.includes('Success') ? 'success' : 'error',
+      });
     });
-}
+};
 </script>
 <style scoped>
 .swal2-container {
